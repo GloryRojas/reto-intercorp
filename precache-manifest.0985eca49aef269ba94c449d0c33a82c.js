0cea68a4c724cdf535d752be29940ece05f61fad
GloryRojas <grojasm94@gmail.com>
2019-07-10 02:39:44 -0500

    Updates

---
+++ precache-manifest.0985eca49aef269ba94c449d0c33a82c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b834d572679e91b9e1da145993045b37",
+    "url": "/reto-intercorp/index.html"
+  },
+  {
+    "revision": "08029c78ca0f5fae9f02",
+    "url": "/reto-intercorp/static/css/main.f2c3949f.chunk.css"
+  },
+  {
+    "revision": "90e5b5b0a69d1b22321f",
+    "url": "/reto-intercorp/static/js/2.4ae8dd4c.chunk.js"
+  },
+  {
+    "revision": "08029c78ca0f5fae9f02",
+    "url": "/reto-intercorp/static/js/main.581d3523.chunk.js"
+  },
+  {
+    "revision": "441c023a7584ad50817a",
+    "url": "/reto-intercorp/static/js/runtime~main.75b142f1.js"
+  }
+]);
